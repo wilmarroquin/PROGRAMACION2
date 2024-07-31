0b9d049d14a9a5a8f8acf3793adb9dbc6e2f50e0
Wilmer Marroquin <ios8.wil@gmail.com>
2024-07-30 20:23:52 -0600

    Ejercicio Constructores

---
+++ POO2/src/modelo/Cliente.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author WILMER
+ */
+public class Cliente extends Persona{
+    private String nit;
+    
+    public Cliente (){}
+
+    public Cliente(String nit, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.nit = nit;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    
+    @Override
+    public void agregar(){
+    System.out.println("Nit" + getNit());
+    System.out.println("Nombres" + getNombres());
+    System.out.println("Apellidos" + getApellidos());
+    System.out.println("Direccion" + getDireccion());
+    System.out.println("Telefono" + getTelefono());
+    System.out.println("Fecha nacimiento" + getFecha_nacimiento());
+    System.out.println("____________________________");
+    }
+   
+}
